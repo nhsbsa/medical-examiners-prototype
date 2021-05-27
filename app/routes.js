bf3e0f129a59dc219575d881d4fc7ac3692a6344
jolumley <49235579+jolumley@users.noreply.github.com>
2021-05-27 08:35:27 +0100

    coroner notification iteration 2

--- app/routes.js
+++ app/routes.js
@@ -7,13 +7,20 @@ const router = express.Router();
 router.use('/me-journey', require('./views/me-journey/_routes'));
 router.use('/meo-journey', require('./views/meo-journey/_routes'));
 
+// Coroner referral routes //
+router.get(/referralMethod/, function (req, res) {
+  if (req.query.radioInlineGroup === "Yes" ) {
+    res.redirect('coroner-ref'); // if both input values are "Yes" - redirect to 'page-name' //
+  } else {
+    res.redirect('external-referral'); // if both input values are "Yes" - redirect to 'page-name' //
+  }
+});
 
-  
 
 
 
 
-  
+
 
 
 module.exports = router;
